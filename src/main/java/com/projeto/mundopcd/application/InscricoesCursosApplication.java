f8c69369bbffd1c0b4dffaeac6a32890e77806e3
eunetox77x <haitecimports@hotmail.com>
2025-03-12 11:34:19 -0300

    commit miguel

---
+++ src/main/java/com/projeto/mundopcd/application/InscricoesCursosApplication.java
@@ -0,0 +1,31 @@
+package com.projeto.mundopcd.application;
+
+import com.projeto.mundopcd.facade.InscricoesCursosFacade;
+import com.projeto.mundopcd.models.InscricoesCursos;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class InscricoesCursosApplication {
+
+    @Autowired
+    private InscricoesCursosFacade facade;
+
+    public InscricoesCursos salvar(InscricoesCursos inscricao) {
+        return facade.salvar(inscricao);
+    }
+
+    public List<InscricoesCursos> buscarTodos() {
+        return facade.buscarTodos();
+    }
+
+    public InscricoesCursos buscarPorId(int id) {
+        return facade.buscarPorId(id).orElse(null);
+    }
+
+    public void deletar(int id) {
+        facade.deletar(id);
+    }
+}
